8f30f44b3b7c0c74d9782d9b7aa1a64800619057
chienli <chienli@chienlideMacBook-Pro.local>
2021-08-07 00:58:53 +0800

    add 新增员工

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -6,37 +6,53 @@
         <template slot="after">
           <el-button size="small" type="warning">导入</el-button>
           <el-button size="small" type="danger">导出</el-button>
-          <el-button size="small" type="primary">新增员工</el-button>
+          <el-button icon="plus" type="primary" size="small" @click="showDialog = true">新增员工</el-button>
         </template>
       </page-tools>
       <!-- 放置表格和分页 -->
-            <el-card v-loading="loading">
+      <el-card v-loading="loading">
         <el-table border :data="list">
           <el-table-column label="序号" sortable="" type="index" />
           <el-table-column label="姓名" sortable="" prop="username" />
           <el-table-column label="工号" sortable="" prop="workNumber" />
           <!-- <el-table-column label="聘用形式" sortable="" prop="formOfEmployment" /> -->
-          <el-table-column label="聘用形式" sortable :formatter="formatEmployment" />
+          <el-table-column
+            label="聘用形式"
+            sortable
+            :formatter="formatEmployment"
+          />
           <el-table-column label="部门" sortable="" prop="departmentName" />
           <!-- <el-table-column label="入职时间" sortable="" prop="timeOfEntry" /> -->
           <el-table-column label="入职时间" sortable="" align="center">
             <!-- 作用域插槽 -->
-            <template slot-scope="{ row }">{{ row.timeOfEntry | formatDate }}</template>
-       </el-table-column>
+            <template slot-scope="{ row }">{{
+              row.timeOfEntry | formatDate
+            }}</template>
+          </el-table-column>
           <el-table-column label="账户状态" sortable="" prop="enableState" />
           <el-table-column label="操作" sortable="" fixed="right" width="280">
-            <template>
+            <template slot-scope="{ row }">
               <el-button type="text" size="small">查看</el-button>
               <el-button type="text" size="small">转正</el-button>
               <el-button type="text" size="small">调岗</el-button>
               <el-button type="text" size="small">离职</el-button>
               <el-button type="text" size="small">角色</el-button>
-              <el-button type="text" size="small">删除</el-button>
+              <el-button
+                type="text"
+                size="small"
+                @click="deleteEmployee(row.id)"
+                >删除</el-button
+              >
             </template>
           </el-table-column>
         </el-table>
         <!-- 分页组件 -->
-        <el-row type="flex" justify="center" align="middle" style="height: 60px">
+        <el-row
+          type="flex"
+          justify="center"
+          align="middle"
+          style="height: 60px"
+        >
           <el-pagination
             layout="prev, pager, next"
             :page-size="page.size"
@@ -46,12 +62,15 @@
           />
         </el-row>
       </el-card>
+      <add-employee v-model="showDialog" />
     </div>
   </div>
 </template>
 <script>
 import { getEmployeeList } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees.js'
+import {delEmployee ,addEmployee} from '@/api/employees'
+import AddEmployee from './components/addEmployee.vue' 
 export default {
   data() {
     return {
@@ -61,14 +80,30 @@ export default {
         page: 1, // 当前页码
         size: 10,
         total: 0 // 总数
-      }
+      },
+      showDialog:false // 新增角色功能
 
     }
+  },
+  components:{
+    AddEmployee
   },
   created() {
     this.getEmployeeList()
   },
   methods: {
+    // 删除员工
+    async deleteEmployee(id) {
+      try {
+        await this.$confirm('您确定删除该员工吗')
+        await delEmployee(id)
+        this.getEmployeeList()
+        this.$message.success('删除员工成功')
+      } catch (error) { 
+
+        console.log(error)
+      }
+    },
     // 格式化聘用形式
     formatEmployment(row, column, cellValue, index) {
       // console.log(row,column,cellValue,'callvalue')
